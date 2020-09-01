e5df36d8dcf0185cbd7ba218e04c1ebb06aa1989
Nicolas HENRY <icewil@gmail.com>
2020-09-02 00:22:30 +0200

    docs: add dynamic compression example

---
+++ examples/dynamic-compression.ts
@@ -0,0 +1,24 @@
+
+import { join } from 'path';
+
+import express from 'express';
+
+import { FileSystemStorage } from '../src/send-stream';
+
+const app = express();
+
+const storage = new FileSystemStorage(join(__dirname, 'assets'), { dynamicCompression: true });
+
+app.get('*', async (req, res, next) => {
+	try {
+		const result = await storage.prepareResponse(req.url, req);
+		result.send(res);
+	} catch (err: unknown) {
+		// eslint-disable-next-line node/callback-return
+		next(err);
+	}
+});
+
+app.listen(3000, () => {
+	console.info('listening on http://localhost:3000');
+});
